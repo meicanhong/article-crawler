cf7bb9ee1691b62d2b710f5dc118bec97e075f0c
danny <canhong5357003@gmail.com>
2024-01-17 18:24:16 +0800

    feat:抛出爬虫异常

--- article_crawler.py
+++ article_crawler.py
@@ -1,5 +1,6 @@
 import logging
 import threading
+from concurrent.futures import wait, FIRST_COMPLETED
 from concurrent.futures.thread import ThreadPoolExecutor
 from datetime import datetime
 from queue import Queue
@@ -43,6 +44,7 @@ class ArticleCrawler:
         self.raw_data_collection = mongo_client["ai_qa"]["crawler_raw_data"]
 
     def run(self):
+        futures = []
         self.url_queue.put(self.url)
         with ThreadPoolExecutor() as executor:
             while self.sites_count < self.max_sites:
@@ -51,28 +53,37 @@ class ArticleCrawler:
                 except:
                     current_url = None
                 if current_url:
-                    executor.submit(self.process_single_url, current_url)
+                    future = executor.submit(self.process_single_url, current_url)
+                    futures.append(future)
                 else:
+                    done, not_done = wait(futures, timeout=3, return_when=FIRST_COMPLETED)
+                    for future in done:
+                        future.result()
+                        futures.remove(future)
                     sleep(5)
 
     def process_single_url(self, url):
-        if url in self.visited_urls:
-            return
-        if self.sites_count >= self.max_sites:
-            return
-        # 下载网页
-        content = self.download_url(url)
-        # 记录已经访问过的URL
-        self.increase_sites_count()
-        self.visited_urls.add(url)
-        # 将当前页面中的链接加入到队列中
-        links = self.extract_links(html=content, url=url, only_internal=self.crawler_only_internal)
-        for link in links:
-            self.add_urls(link)
-        # 保存原始的HTML
-        self.save_raw_html(url, content)
-        # 解析网页
-        self.parse_website(url, content)
+        try:
+            if url in self.visited_urls:
+                return
+            if self.sites_count >= self.max_sites:
+                return
+            # 下载网页
+            content = self.download_url(url)
+            # 记录已经访问过的URL
+            self.increase_sites_count()
+            self.visited_urls.add(url)
+            # 将当前页面中的链接加入到队列中
+            links = self.extract_links(html=content, url=url, only_internal=self.crawler_only_internal)
+            for link in links:
+                self.add_urls(link)
+            # 保存原始的HTML
+            self.save_raw_html(url, content)
+            # 解析网页
+            self.parse_website(url, content)
+        except Exception as e:
+            logging.error(e)
+            raise e
 
     def download_url(self, current_url):
         browser = self.tls.playwright.chromium.launch(
@@ -191,5 +202,5 @@ class ArticleCrawler:
 
 if __name__ == '__main__':
     url = 'https://followin.io/en'
-    crawler = ArticleCrawler(url=url, max_sites=1, max_links_per_page=999, timeout=120, crawler_only_internal=False)
+    crawler = ArticleCrawler(url=url, max_sites=100, max_links_per_page=999, timeout=120, crawler_only_internal=False)
     crawler.run()
