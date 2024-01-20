b992f8b056fa87c93fd000e1f0497767a34264fd
DannyM <canhong5357003@gmail.com>
2024-01-20 16:55:52 +0800

    feat:爬虫支持黑名单功能&过滤低质量url

--- article_crawler.py
+++ article_crawler.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import threading
 from concurrent.futures import wait, FIRST_COMPLETED
 from concurrent.futures.thread import ThreadPoolExecutor
@@ -15,11 +16,12 @@ from newspaper import Article
 from playwright.sync_api import sync_playwright
 
 from base_etl_item import BaseETLItem
+from config import project_config
 from constant import PROJECT_PATH
 
 logging.basicConfig(level=logging.INFO)
 
-mongo_client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+mongo_client = pymongo.MongoClient(project_config.mongodb_url)
 
 
 class Tls(threading.local):
@@ -31,8 +33,10 @@ class Tls(threading.local):
 class ArticleCrawler:
     tls = Tls()
 
-    def __init__(self, url, max_sites=50, max_links_per_page=10, timeout=60, crawler_only_internal=True):
+    def __init__(self, url, type='article', max_sites=50, max_links_per_page=10, timeout=60, crawler_only_internal=True,
+                 match_url=None, black_website=None):
         self.url = url
+        self.type = type
         self.max_sites = max_sites
         self.max_links_per_page = max_links_per_page
         self.visited_urls = set()
@@ -41,13 +45,15 @@ class ArticleCrawler:
         self.lock = threading.Lock()
         self.timeout = timeout
         self.crawler_only_internal = crawler_only_internal
+        self.match_url = match_url
+        self.black_website = black_website
         self.etl_data_collection_name = 'crawler_etl_data'
         self.raw_data_collection = mongo_client["ai_qa"]["crawler_raw_data"]
 
     def run(self):
         futures = []
         self.url_queue.put(self.url)
-        with ThreadPoolExecutor() as executor:
+        with ThreadPoolExecutor(max_workers=1) as executor:
             while self.sites_count < self.max_sites:
                 try:
                     current_url = self.url_queue.get_nowait()
@@ -69,26 +75,33 @@ class ArticleCrawler:
                 return
             if self.sites_count >= self.max_sites:
                 return
+            logging.info(f"Processing {url}")
             # 下载网页
             content = self.download_url(url)
             # 记录已经访问过的URL
-            self.increase_sites_count()
             self.visited_urls.add(url)
             # 将当前页面中的链接加入到队列中
-            links = self.extract_links(html=content, url=url, only_internal=self.crawler_only_internal)
+            links = self.extract_links(html=content, url=url)
             for link in links:
                 self.add_urls(link)
             # 保存原始的HTML
             self.save_raw_html(url, content)
             # 解析网页
-            self.parse_website(url, content)
+            doc = self.parse_website(url, content)
+            # 黑名单过滤, 不保存黑名单中的网站
+            black_pattern = re.compile(self.black_website)
+            if black_pattern.match(url):
+                return None
+            # 保存解析后的数据
+            doc.update_one()
+            self.increase_sites_count()
         except Exception as e:
             logging.error(e)
             raise e
 
     def download_url(self, current_url):
         browser = self.tls.playwright.chromium.launch(
-            headless=True,
+            headless=False,
             args=['--disable-blink-features=AutomationControlled']  # 防止被检测
         )
         context = browser.new_context(
@@ -101,13 +114,18 @@ class ArticleCrawler:
 
         page = context.new_page()
 
-        page.goto(current_url, timeout=self.timeout * 1000)
-        # 滚动到指定高度
-        scroll_height = 200000  # 替换为您想要的滚动高度
-        page.evaluate(f"window.scrollTo(0, {scroll_height});")
-        # 等待一段时间
-        wait_time = 3000  # 替换为您想要等待的时间（毫秒）
-        page.wait_for_timeout(wait_time)
+        page.goto(current_url, timeout=self.timeout * 1000, wait_until='domcontentloaded')
+
+        scroll_height = 30000  # 替换为您想要的滚动高度
+        scroll_height_unit = 10000  # 替换为您想要的滚动高度单位
+        current_height = 0
+        for i in range(0, scroll_height, scroll_height_unit):
+            current_height += scroll_height_unit
+            # 滚动到指定高度
+            page.evaluate(f"window.scrollTo(0, {current_height});")
+            # 等待一段时间
+            wait_time = 1000  # 替换为您想要等待的时间（毫秒）
+            page.wait_for_timeout(wait_time)
 
         content = page.content()
         page.close()
@@ -124,7 +142,7 @@ class ArticleCrawler:
         self.raw_data_collection.update_one(filter={"url": url}, update={"$set": doc},
                                             upsert=True)
 
-    def extract_links(self, url, html, only_internal=True):
+    def extract_links(self, url, html):
         soup = BeautifulSoup(html, 'html.parser')
         tags_to_remove = ['nav', 'footer', 'script', 'style', 'noscript', 'svg']
         # 移除指定的标签
@@ -144,9 +162,17 @@ class ArticleCrawler:
                 continue
             href = scrub_url(href)
             href = clean_url(href)
-            if only_internal and is_external(href, self.url):
+            if self.crawler_only_internal and is_external(href, self.url):
                 continue
             if href in self.visited_urls:
+                continue
+            if self.match_url is not None:
+                url_pattern = re.compile(self.match_url)
+                if not url_pattern.match(href):
+                    continue
+            # path长度小于15，大概率不是文章
+            url_obj = urlparse(href)
+            if url_obj.path is None or len(url_obj.path) < 10:
                 continue
             urls.append(href)
 
@@ -154,7 +180,7 @@ class ArticleCrawler:
         urls.sort(key=lambda x: len(x), reverse=True)
         urls = urls[:self.max_links_per_page]
 
-        logging.info(f"Crawled Queue: {self.url_queue.qsize()}, Visited: {len(self.visited_urls)}")
+        logging.info(f"Crawled Queue:{self.url_queue.qsize()}, Success: {self.sites_count}")
 
         return urls
 
@@ -164,9 +190,7 @@ class ArticleCrawler:
         article.parse()
         published_date = article.publish_date
         if published_date is None:
-            published_date = find_date(content)
-        logging.info(f"URL: {url} Title: {article.title}, Published Date: {published_date}")
-        logging.info(f"Text: {article.text[:100]}...\n")
+            published_date = find_date(htmlobject=content, outputformat="%Y-%m-%d %H:%M:%S")
 
         text = trafilatura.extract(filecontent=content, include_comments=True, include_images=False)
         metadata = trafilatura.extract_metadata(filecontent=content)
@@ -174,25 +198,36 @@ class ArticleCrawler:
         if tags is None or len(tags) == 0:
             tags = metadata.tags
 
+        author = metadata.author
+        if author is None or len(author) == 0:
+            if metadata.author is not None:
+                author = article.author
+            else:
+                element = BeautifulSoup(content, 'html.parser').find('span', class_='text-text1 text-lg font-medium')
+                if element is not None:
+                    author = element.text
+
         doc = BaseETLItem(collection_name=self.etl_data_collection_name)
         doc.website = url
         doc.website_url = url
         doc.headline = article.title
         doc.description = article.text[:200]
         doc.contents = [{"type": "text", "content": text}]
-        doc.author = article.authors
-        doc.source = urlparse(url).netloc
+        doc.author = author
+        doc.source = self.type
         doc.language = article.meta_lang
         doc.tags = tags
         doc.created_at = datetime.utcnow()
         doc.updated_at = datetime.utcnow()
         doc.published_at = published_date
-        doc.update_one()
+        return doc
 
     def add_urls(self, url):
         if self.sites_count >= self.max_sites:
             return
         if url in self.visited_urls:
+            return
+        if self.url_queue.qsize() >= self.max_sites * 2:
             return
         self.url_queue.put(url, block=False)
 
@@ -203,5 +238,6 @@ class ArticleCrawler:
 
 if __name__ == '__main__':
     url = 'https://followin.io/en'
-    crawler = ArticleCrawler(url=url, max_sites=100, max_links_per_page=999, timeout=120, crawler_only_internal=False)
+    crawler = ArticleCrawler(url=url, type='news', max_sites=100, max_links_per_page=999,
+                             timeout=60, crawler_only_internal=False)
     crawler.run()
